2175d50b21db876dda4fd46451ed83dc0aaa2153
DJDrama <putzzang@naver.com>
2021-09-29 22:40:39 +0900

    Initial Commit

---
+++ DIT 스포츠클럽/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DIT 스포츠클럽
+//
+//  Created by dj on 2021/09/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DIT 스포츠클럽/DIT_____________App.swift
@@ -0,0 +1,17 @@
+//
+//  DIT_____________App.swift
+//  DIT 스포츠클럽
+//
+//  Created by dj on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct DIT_____________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
